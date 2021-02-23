8121c0f8e73bc59763c358309654b7c5e6c99636
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-23 19:52:33 +0500

    Inital Commit

---
+++ packages/www/.cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("D:\\web\\bootcamp\\gatsby-todo-app\\packages\\www\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-jsx": (preferDefault(require("D:\\web\\bootcamp\\gatsby-todo-app\\packages\\www\\src\\pages\\index.jsx")))
+}
+
