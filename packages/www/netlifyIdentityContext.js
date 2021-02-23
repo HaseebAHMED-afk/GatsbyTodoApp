c35bd46abc056afd90dd790fc3c3b3879ecabd3a
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-23 23:56:23 +0500

    Checkpoint

--- packages/www/netlifyIdentityContext.js
+++ packages/www/netlifyIdentityContext.js
@@ -17,7 +17,7 @@ const IdentityProvider = (props) => {
     netlifyIdentity.init({});
   }, []);
 
-
+     
   netlifyIdentity.on('login' , (user) => {
     netlifyIdentity.close();
     setUser(user);
