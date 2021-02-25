ce2c7163ce3fc26f9fda4f2683ee3d322c51cc84
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-25 21:13:38 +0500

    Changes

--- packages/www/src/pages/app.jsx
+++ packages/www/src/pages/app.jsx
@@ -2,40 +2,9 @@ import React, { useContext } from 'react'
 import { Router , Link} from '@reach/router'
 import { IdentityContext } from '../../netlifyIdentityContext'
 import { Flex , Heading , Button, Container , NavLink } from 'theme-ui'
-// import { Link } from 'gatsby'
+import Dash from '../components/dashboard'
 
 
-let Dash = () => {
-
-    const {user,identity } = useContext( IdentityContext )
-
-    return (
-        <Container>
-        <Flex as="nav">
-        <NavLink as={Link} to="/" p={2}>
-          Home
-        </NavLink>
-        <NavLink as={Link} to="/app" p={2}>
-          Dashboard
-        </NavLink>
-        {
-            user && <NavLink  p={2}
-                onClick={()=>{
-                    identity.logout()
-                }}
-            
-            >
-                Log Out
-            {
-               user.user_metadata.full_name
-            }
-        </NavLink>
-        }
-        
-      </Flex>
-      </Container>
-    )
-}
 
 let DashLoggedOut = () => {
     const {user , identity} = useContext( IdentityContext )
